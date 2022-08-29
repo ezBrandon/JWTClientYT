fa6cc1c790f1f1e59f94dfeb1c37d838bbcfc687
Israel <ifq130030@gmail.com>
2022-08-29 16:46:39 -0500

    commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { MaterialModule } from './material/material.module';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent, pathMatch: 'full'},
+      { path: 'logout', component: LogoutComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, NgZone } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CredentialResponse, PromptMomentNotification } from 'google-one-tap';
+import { environment } from 'src/environments/environment';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  private clientId = environment.clientId
+
+  constructor(
+    private router: Router,
+    private service: AuthService,
+    private _ngZone: NgZone) { }
+
+    ngOnInit(): void {
+      // @ts-ignore
+      window.onGoogleLibraryLoad = () => {
+        // @ts-ignore
+        google.accounts.id.initialize({
+          client_id: this.clientId,
+          callback: this.handleCredentialResponse.bind(this),
+          auto_select: false,
+          cancel_on_tap_outside: true
+        });
+        // @ts-ignore
+        google.accounts.id.renderButton(
+        // @ts-ignore
+        document.getElementById("buttonDiv"),
+          { theme: "outline", size: "large", width: "100%" } 
+        );
+        // @ts-ignore
+        google.accounts.id.prompt((notification: PromptMomentNotification) => {});
+      };
+    }
+
+    async handleCredentialResponse(response: CredentialResponse) {
+      debugger;
+      await this.service.LoginWithGoogle(response.credential).subscribe(
+        (x:any) => {
+          debugger;
+          localStorage.setItem("token", x.token);
+          this._ngZone.run(() => {
+            this.router.navigate(['/logout']);
+          })},
+        (error:any) => {
+            console.log(error);
+          }
+        );  
+  }
+}
